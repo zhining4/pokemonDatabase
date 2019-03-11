a382cd989b8e70cd6afd40bd8cea519250d4ca18
Yifan Cao <amadeus_cao.cs@gmail.com>
2019-03-10 20:02:43 -0500

    deploying

--- my-app/src/Component/App/App.js
+++ my-app/src/Component/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import Search from '../Search/search.jsx';
 import Detail from '../Detail/detail.jsx';
-//import history from '../History/history.jsx'; 
 import Gallery from '../Gallery/gallery.jsx';
 
 class App extends Component {
@@ -10,7 +9,7 @@ class App extends Component {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path = '/CS498RK_mp2' component={ Search }/>
+          <Route exact path = '/' component={ Search }/>
           <Route exact path = '/Pokemon' component = { Gallery }/>
           <Route exact path = '/Pokemon/:id' component = { Detail }/>
         </Switch>

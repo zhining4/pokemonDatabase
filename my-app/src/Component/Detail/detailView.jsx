1d94f483d3670d7efb0e192b442774463aaa8d18
Yifan Cao <amadeus_cao.cs@gmail.com>
2019-03-11 22:31:27 -0500

    changing filter

--- my-app/src/Component/Detail/detailView.jsx
+++ my-app/src/Component/Detail/detailView.jsx
@@ -43,11 +43,11 @@ class DetailView extends Component {
                         />
                         <div className = {DetailText}>
                             <div className = {DetailSection}>
-                                Height: {this.props.pokemon.height * 3.93701}'
+                                Height: {this.props.pokemon.height * 3.94}'
                             </div>
 
                             <div className = {DetailSection}>
-                                Weight: {this.props.pokemon.weight * 0.220462} lbs
+                                Weight: {this.props.pokemon.weight * 0.22} lbs
                             </div>
 
                             <div className = {DetailSection}>
